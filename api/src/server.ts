d1006285588b1b7155cc5abd5173b25295e422f5
Joao Andrade <joaovandrade30@gmail.com>
2026-01-05 01:33:29 -0300

    update on server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import authRoutes from './routes/auth'
+const PORT = process.env.PORT || 3333
 
 const app = express()
 
@@ -9,6 +10,6 @@ app.use(express.json())
 
 app.use('/auth', authRoutes)
 
-app.listen(3333, () => {
-  console.log('🚀 Backend rodando em http://localhost:3333')
+app.listen(PORT, () => {
+  console.log(`🚀 Backend rodando em http://localhost:${PORT}`)
 })
