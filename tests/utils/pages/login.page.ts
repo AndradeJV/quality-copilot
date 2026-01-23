c249e85cf8cd1a9143098be2913c2882e2fdae90
João Vitor Andrade de Araujo <72886034+AndradeJV@users.noreply.github.com>
2026-01-23 17:32:54 -0300

    feat(tests): adiciona login.page.ts

---
+++ tests/utils/pages/login.page.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+import { Page, Locator } from '@playwright/test';
+
+export class LoginPage {
+	readonly page: Page;
+	readonly emailInput: Locator;
+	readonly passwordInput: Locator;
+	readonly submitButton: Locator;
+	readonly loadingText: Locator;
+
+	constructor(page: Page) {
+		this.page = page;
+		this.emailInput = page.getByTestId('email-input');
+		this.passwordInput = page.getByTestId('password-input');
+		this.submitButton = page.getByRole('button', { name: 'Entrar' });
+		this.loadingText = page.getByText('testando...');
+	}
+
+	async goto() {
+		await this.page.goto('/login');
+	}
+
+	async login(email: string, password: string) {
+		await this.emailInput.fill(email);
+		await this.passwordInput.fill(password);
+		await this.submitButton.click();
+	}
+
+	async isLoadingVisible() {
+		return await this.loadingText.isVisible();
+	}
+}
